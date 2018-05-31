af84382c96aa2b348209a435bc340ba8d4a2dc91
Hao <qfgaohao@gmail.com>
2018-05-31 14:26:32 +1200

    add a general model pruning impl

---
+++ vision/prunning/prunner.py
@@ -0,0 +1,146 @@
+import torch
+import torch.nn as nn
+import logging
+
+from ..utils.model_book import ModelBook
+
+
+class ModelPrunner:
+    def __init__(self, model, train_fun):
+        """ Implement the pruning algorithm described in the paper https://arxiv.org/pdf/1611.06440.pdf .
+
+        The prunning criteria is dC/dh * h, while C is the cost, h is the activation.
+        """
+        self.model = model
+        self.train_fun = train_fun
+        self.book = ModelBook(self.model)
+        self.outputs = {}
+        self.grads = {}
+        self.handles = []
+        self.batch_norms = {}
+        self.last_conv = None
+        self.convs = {}
+
+    def _make_new_conv(self, conv, filter_index, channel_type="out"):
+        if not isinstance(conv, nn.Conv2d):
+            raise TypeError(f"The module is not Conv2d, but {type(conv)}.")
+
+        if channel_type == "out":
+            new_conv = nn.Conv2d(conv.in_channels, conv.out_channels - 1, conv.kernel_size, conv.stride,
+                                 conv.padding, conv.dilation, conv.groups, conv.bias is not None)
+            if filter_index == 0:
+                new_conv.weight.data = conv.weight.data[filter_index + 1:]
+                if conv.bias is not None:
+                    new_conv.bias.data = conv.bias.data[filter_index + 1:]
+            elif filter_index == conv.weight.data.size(0) - 1:
+                new_conv.weight.data = conv.weight.data[:filter_index]
+                if conv.bias is not None:
+                    new_conv.bias.data = conv.bias.data[:filter_index]
+            else:
+                new_conv.weight.data = torch.cat([
+                    conv.weight.data[:filter_index],
+                    conv.weight.data[filter_index + 1:]
+                ])
+                if conv.bias is not None:
+                    new_conv.bias.data = torch.cat([
+                        conv.bias.data[:filter_index],
+                        conv.bias.data[filter_index + 1:]
+                    ])
+        elif channel_type == 'in':
+            new_conv = nn.Conv2d(conv.in_channels - 1, conv.out_channels, conv.kernel_size, conv.stride,
+                                 conv.padding, conv.dilation, conv.groups, conv.bias is not None)
+            if filter_index == 0:
+                new_conv.weight.data = conv.weight.data[:, filter_index + 1:, :, :]
+            elif filter_index == conv.weight.data.size(1) - 1:
+                new_conv.weight.data = conv.weight.data[:, :filter_index, :, :]
+            else:
+                new_conv.weight.data = torch.cat([
+                    conv.weight.data[:, :filter_index, :, :],
+                    conv.weight.data[:, filter_index + 1:, :, :]
+                ], dim=1)
+            if conv.bias is not None:
+                new_conv.bias.data = conv.bias.data
+        else:
+            raise ValueError(f"{channel_type} should be either 'in' or 'out'.")
+        return new_conv
+
+    def remove_filter(self, conv, filter_index):
+        if not isinstance(conv, nn.Conv2d):
+            raise TypeError(f"The module is not Conv2d, but {type(conv)}.")
+
+        new_conv = self._make_new_conv(conv, filter_index, channel_type="out")
+        path = self.book.get_path(conv)
+        logging.info(f"Prune filter {filter_index} on Conv2d {conv} at path {'/'.join(path)}")
+        conv_parent = self.book.get_module(path[:-1])
+        conv_parent._modules[path[-1]] = new_conv
+        self.book.update(path, new_conv)
+
+        next_conv = self.convs.get(conv)
+        if next_conv is not None:
+            new_next_conv = self._make_new_conv(next_conv, filter_index, channel_type="in")
+            next_path = self.book.get_path(next_conv)
+            next_parent = self.book.get_module(next_path[:-1])
+            next_parent._modules[next_path[-1]] = new_next_conv
+            self.book.update(next_path, new_next_conv)
+
+        # reduce the out channels of batch norm
+        batch_norm = self.batch_norms.get(conv)
+        if batch_norm:
+            new_batch_norm = nn.BatchNorm2d(batch_norm.num_features - 1)
+            batch_norm_path = self.book.get_path(batch_norm)
+            batch_norm_parent = self.book.get_module(batch_norm_path[:-1])
+            batch_norm_parent._modules[batch_norm_path[-1]] = new_batch_norm
+            self.book.update(batch_norm_path, new_batch_norm)
+
+    def prune(self, ignored_paths=[]):
+        """Prune one conv2d filter.
+        """
+        self.register_hooks(ignored_paths)
+        self.train_fun(self.model)
+        ranks = []
+        for m, output in self.outputs.items():
+            output = output.data
+            grad = self.grads[m].data
+            v = grad * output
+            v = v.sum(0).sum(1).sum(1)  # sum to the channel axis.
+            v = v / torch.sqrt(torch.sum(v * v))  # normalize
+            for i, e in enumerate(v):
+                ranks.append((m, i, e))
+        module, filter_index, value = min(ranks, key=lambda t: t[2])
+        self.remove_filter(module, filter_index)
+        self.deregister_hooks()
+
+    def register_hooks(self, ignored_paths=[]):
+        """Run register before training for pruning."""
+        ignored_modules = set(self.book.get_module(path) for path in ignored_paths)
+        self.outputs.clear()
+        self.grads.clear()
+        self.handles.clear()
+        self.last_conv = None
+        self.batch_norms.clear()
+        self.convs.clear()
+
+        def forward_hook(m, input, output):
+            if isinstance(m, nn.Conv2d):
+                if m not in ignored_modules:
+                    self.outputs[m] = output
+                if self.last_conv:
+                    self.convs[self.last_conv] = m
+                self.last_conv = m
+            elif isinstance(m, nn.BatchNorm2d):
+                if self.last_conv:
+                    self.batch_norms[self.last_conv] = m
+
+        def backward_hook(m, input, output):
+            self.grads[m] = output[0]
+
+        for path, m in self.book.modules(module_type=(nn.Conv2d, nn.BatchNorm2d)):
+            h = m.register_forward_hook(forward_hook)
+            self.handles.append(h)
+            h = m.register_backward_hook(backward_hook)
+            self.handles.append(h)
+
+    def deregister_hooks(self):
+        """Run degresiter before retraining to recover the model"""
+        for handle in self.handles:
+            handle.remove()
---
+++ vision/utils/model_book.py
@@ -0,0 +1,74 @@
+from collections import OrderedDict
+import torch.nn as nn
+
+
+class ModelBook:
+    """Maintain the mapping between modules and their paths.
+
+    Example:
+        book = ModelBook(model_ft)
+        for p, m in book.conv2d_modules():
+            print('path:', p, 'num of filters:', m.out_channels)
+            assert m is book.get_module(p)
+    """
+
+    def __init__(self, model):
+        self._model = model
+        self._modules = OrderedDict()
+        self._paths = OrderedDict()
+        path = []
+        self._construct(self._model, path)
+
+    def _construct(self, module, path):
+        if not module._modules:
+            return
+        for name, m in module._modules.items():
+            cur_path = tuple(path + [name])
+            self._paths[m] = cur_path
+            self._modules[cur_path] = m
+            self._construct(m, path + [name])
+
+    def conv2d_modules(self):
+        return self.modules(nn.Conv2d)
+
+    def linear_modules(self):
+        return self.modules(nn.Linear)
+
+    def modules(self, module_type=None):
+        for p, m in self._modules.items():
+            if not module_type or isinstance(m, module_type):
+                yield p, m
+
+    def num_of_conv2d_modules(self):
+        return self.num_of_modules(nn.Conv2d)
+
+    def num_of_conv2d_filters(self):
+        """Return the sum of out_channels of all conv2d layers.
+
+        Here we treat the sub weight with size of [in_channels, h, w] as a single filter.
+        """
+        num_filters = 0
+        for _, m in self.conv2d_modules():
+            num_filters += m.weight.size(0)
+        return num_filters
+
+    def num_of_linear_modules(self):
+        return self.num_of_modules(nn.Linear)
+
+    def num_of_modules(self, module_type=None):
+        num = 0
+        for p, m in self._modules.items():
+            if not module_type or isinstance(m, module_type):
+                num += 1
+
+    def get_module(self, path):
+        return self._modules.get(path)
+
+    def get_path(self, module):
+        return self._paths.get(module)
+
+    def update(self, path, module):
+        old_module = self._modules[path]
+        del self._paths[old_module]
+        self._paths[module] = path
+        self._modules[path] = module
