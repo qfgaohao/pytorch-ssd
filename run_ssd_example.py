f523cb5bf0430b6407bbea9544b26dc65e6397ff
Hao <qfgaohao@gmail.com>
2018-09-10 16:55:41 +1200

    make it work for both voc dataset and open image dataset.

--- run_ssd_example.py
+++ run_ssd_example.py
@@ -1,19 +1,20 @@
 from vision.ssd.vgg_ssd import create_vgg_ssd, create_vgg_ssd_predictor
 from vision.ssd.mobilenetv1_ssd import create_mobilenetv1_ssd, create_mobilenetv1_ssd_predictor
-from vision.datasets import voc_dataset
 from vision.utils.misc import Timer
 import cv2
 import sys
 
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 5:
     print('Usage: python run_ssd_example.py <net type>  <model path> <image path>')
     sys.exit(0)
 net_type = sys.argv[1]
 model_path = sys.argv[2]
-image_path = sys.argv[3]
+label_path = sys.argv[3]
+image_path = sys.argv[4]
 
-num_classes = len(voc_dataset.class_names)
+class_names = [name.strip() for name in open(label_path).readlines()]
+num_classes = len(class_names)
 if net_type == "mobilenet-v1-ssd":
     net = create_mobilenetv1_ssd(num_classes, is_test=True)
 else:
@@ -31,7 +32,8 @@ boxes, labels, probs = predictor.predict(image, 10, 0.4)
 for i in range(boxes.size(0)):
     box = boxes[i, :]
     cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
-    label = f"""{voc_dataset.class_names[labels[i]]}: {probs[i]:.2f}"""
+    #label = f"""{voc_dataset.class_names[labels[i]]}: {probs[i]:.2f}"""
+    label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
     cv2.putText(orig_image, label,
                 (box[0] + 20, box[1] + 40),
                 cv2.FONT_HERSHEY_SIMPLEX,
