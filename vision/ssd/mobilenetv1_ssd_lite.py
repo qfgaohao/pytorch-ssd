a3b1da2097c20d63c4f65137b090f9a8116a67c5
Hao <qfgaohao@gmail.com>
2018-10-25 10:28:09 +1300

    change default device from cpu to GPU if GPU is available

--- vision/ssd/mobilenetv1_ssd_lite.py
+++ vision/ssd/mobilenetv1_ssd_lite.py
@@ -70,7 +70,7 @@ def create_mobilenetv1_ssd_lite(num_classes, is_test=False):
                extras, classification_headers, regression_headers, is_test=is_test, config=config)
 
 
-def create_mobilenetv1_ssd_lite_predictor(net, candidate_size=200, nms_method=None, sigma=0.5, device=torch.device('cpu')):
+def create_mobilenetv1_ssd_lite_predictor(net, candidate_size=200, nms_method=None, sigma=0.5, device=None):
     predictor = Predictor(net, config.image_size, config.image_mean,
                           config.image_std,
                           nms_method=nms_method,
