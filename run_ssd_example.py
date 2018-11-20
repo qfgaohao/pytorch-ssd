48d40f52f5892cfa046cab5badc2a4ee5067d7d6
Hao <qfgaohao@gmail.com>
2018-11-21 11:20:58 +1300

    add testing code for mb2-ssd-lite

--- run_ssd_example.py
+++ run_ssd_example.py
@@ -1,5 +1,8 @@
 from vision.ssd.vgg_ssd import create_vgg_ssd, create_vgg_ssd_predictor
 from vision.ssd.mobilenetv1_ssd import create_mobilenetv1_ssd, create_mobilenetv1_ssd_predictor
+from vision.ssd.mobilenetv1_ssd_lite import create_mobilenetv1_ssd_lite, create_mobilenetv1_ssd_lite_predictor
+from vision.ssd.squeezenet_ssd_lite import create_squeezenet_ssd_lite, create_squeezenet_ssd_lite_predictor
+from vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite, create_mobilenetv2_ssd_lite_predictor
 from vision.utils.misc import Timer
 import cv2
 import sys
@@ -14,16 +17,35 @@ label_path = sys.argv[3]
 image_path = sys.argv[4]
 
 class_names = [name.strip() for name in open(label_path).readlines()]
-num_classes = len(class_names)
-if net_type == "mb1-ssd":
-    net = create_mobilenetv1_ssd(num_classes, is_test=True)
+
+if net_type == 'vgg16-ssd':
+    net = create_vgg_ssd(len(class_names), is_test=True)
+elif net_type == 'mb1-ssd':
+    net = create_mobilenetv1_ssd(len(class_names), is_test=True)
+elif net_type == 'mb1-ssd-lite':
+    net = create_mobilenetv1_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'mb2-ssd-lite':
+    net = create_mobilenetv2_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'sq-ssd-lite':
+    net = create_squeezenet_ssd_lite(len(class_names), is_test=True)
 else:
-    net = create_vgg_ssd(num_classes, is_test=True)
+    print("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
+    sys.exit(1)
 net.load(model_path)
-if net_type == "mb1-ssd":
-    predictor = create_mobilenetv1_ssd_predictor(net, candidate_size=200)
-else:
+
+if net_type == 'vgg16-ssd':
     predictor = create_vgg_ssd_predictor(net, candidate_size=200)
+elif net_type == 'mb1-ssd':
+    predictor = create_mobilenetv1_ssd_predictor(net, candidate_size=200)
+elif net_type == 'mb1-ssd-lite':
+    predictor = create_mobilenetv1_ssd_lite_predictor(net, candidate_size=200)
+elif net_type == 'mb2-ssd-lite':
+    predictor = create_mobilenetv2_ssd_lite_predictor(net, candidate_size=200)
+elif net_type == 'sq-ssd-lite':
+    predictor = create_squeezenet_ssd_lite_predictor(net, candidate_size=200)
+else:
+    print("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
+    sys.exit(1)
 
 orig_image = cv2.imread(image_path)
 image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
--- run_ssd_live_demo.py
+++ run_ssd_live_demo.py
@@ -2,13 +2,13 @@ from vision.ssd.vgg_ssd import create_vgg_ssd, create_vgg_ssd_predictor
 from vision.ssd.mobilenetv1_ssd import create_mobilenetv1_ssd, create_mobilenetv1_ssd_predictor
 from vision.ssd.mobilenetv1_ssd_lite import create_mobilenetv1_ssd_lite, create_mobilenetv1_ssd_lite_predictor
 from vision.ssd.squeezenet_ssd_lite import create_squeezenet_ssd_lite, create_squeezenet_ssd_lite_predictor
-
+from vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite, create_mobilenetv2_ssd_lite_predictor
 from vision.utils.misc import Timer
 import cv2
 import sys
 
 if len(sys.argv) < 4:
-    print('Usage: python run_ssd_example.py <net type>  <model path> [video file]')
+    print('Usage: python run_ssd_example.py <net type>  <model path> <label path> [video file]')
     sys.exit(0)
 net_type = sys.argv[1]
 model_path = sys.argv[2]
@@ -31,6 +31,8 @@ elif net_type == 'mb1-ssd':
     net = create_mobilenetv1_ssd(len(class_names), is_test=True)
 elif net_type == 'mb1-ssd-lite':
     net = create_mobilenetv1_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'mb2-ssd-lite':
+    net = create_mobilenetv2_ssd_lite(len(class_names), is_test=True)
 elif net_type == 'sq-ssd-lite':
     net = create_squeezenet_ssd_lite(len(class_names), is_test=True)
 else:
@@ -44,6 +46,8 @@ elif net_type == 'mb1-ssd':
     predictor = create_mobilenetv1_ssd_predictor(net, candidate_size=200)
 elif net_type == 'mb1-ssd-lite':
     predictor = create_mobilenetv1_ssd_lite_predictor(net, candidate_size=200)
+elif net_type == 'mb2-ssd-lite':
+    predictor = create_mobilenetv2_ssd_lite_predictor(net, candidate_size=200)
 elif net_type == 'sq-ssd-lite':
     predictor = create_squeezenet_ssd_lite_predictor(net, candidate_size=200)
 else:
