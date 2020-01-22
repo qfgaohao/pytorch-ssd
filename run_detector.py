4a9ee6f25e4a8097ca27b07a2cc47501333c2f35
Camilo Gonçalves <camilolgon@gmail.com>
2020-01-22 11:58:51 -0300

    Creating the detector class.

--- run_detector.py
+++ run_detector.py
@@ -2,24 +2,8 @@ import sys
 
 import cv2
 
-from vision.ssd.mobilenet_v2_ssd_lite import (
-    create_mobilenetv2_ssd_lite,
-    create_mobilenetv2_ssd_lite_predictor,
-)
-from vision.ssd.mobilenetv1_ssd import (
-    create_mobilenetv1_ssd,
-    create_mobilenetv1_ssd_predictor,
-)
-from vision.ssd.mobilenetv1_ssd_lite import (
-    create_mobilenetv1_ssd_lite,
-    create_mobilenetv1_ssd_lite_predictor,
-)
-from vision.ssd.squeezenet_ssd_lite import (
-    create_squeezenet_ssd_lite,
-    create_squeezenet_ssd_lite_predictor,
-)
-from vision.ssd.vgg_ssd import create_vgg_ssd, create_vgg_ssd_predictor
 from vision.utils.misc import Timer
+from wagon_tracking.detection import WagonDetector
 from wagon_tracking.videostream import VideoFileStream, VideoLiveStream
 from wagon_tracking.transforms import DistortionRectifier
 
@@ -38,49 +22,16 @@ transform = [
     )
 ]
 if len(sys.argv) >= 5:
-    cap = VideoFileStream(sys.argv[4], queue_sz=64, transforms=transform)  # capture from file
+    cap = VideoFileStream(
+        sys.argv[4], queue_sz=64, transforms=transform
+    )  # capture from file
     frame_time = 1
 else:
     cap = VideoLiveStream(transforms=transform)  # capture from camera
     frame_time = int(1 / cap.get(cv2.CAP_PROP_FPS) * 1000)
 cap.start()
 
-class_names = ['BACKGROUND'] + [name.strip() for name in open(label_path).readlines()]
-num_classes = len(class_names)
-
-
-if net_type == 'vgg16-ssd':
-    net = create_vgg_ssd(len(class_names), is_test=True)
-elif net_type == 'mb1-ssd':
-    net = create_mobilenetv1_ssd(len(class_names), is_test=True)
-elif net_type == 'mb1-ssd-lite':
-    net = create_mobilenetv1_ssd_lite(len(class_names), is_test=True)
-elif net_type == 'mb2-ssd-lite':
-    net = create_mobilenetv2_ssd_lite(len(class_names), is_test=True)
-elif net_type == 'sq-ssd-lite':
-    net = create_squeezenet_ssd_lite(len(class_names), is_test=True)
-else:
-    print(
-        "The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite."
-    )
-    sys.exit(1)
-net.load(model_path)
-
-if net_type == 'vgg16-ssd':
-    predictor = create_vgg_ssd_predictor(net, candidate_size=200)
-elif net_type == 'mb1-ssd':
-    predictor = create_mobilenetv1_ssd_predictor(net, candidate_size=200)
-elif net_type == 'mb1-ssd-lite':
-    predictor = create_mobilenetv1_ssd_lite_predictor(net, candidate_size=200)
-elif net_type == 'mb2-ssd-lite':
-    predictor = create_mobilenetv2_ssd_lite_predictor(net, candidate_size=200)
-elif net_type == 'sq-ssd-lite':
-    predictor = create_squeezenet_ssd_lite_predictor(net, candidate_size=200)
-else:
-    print(
-        "The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite."
-    )
-    sys.exit(1)
+detector = WagonDetector(net_type, label_path, model_path)
 
 cv2.namedWindow('annotated', cv2.WINDOW_NORMAL)
 
@@ -89,14 +40,13 @@ while cap.more():
     orig_image = cap.read()
     if orig_image is None:
         continue
-    image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
     timer.start()
-    boxes, labels, probs = predictor.predict(image, 10, 0.4)
+    boxes, labels, probs = detector.run(orig_image, 10, 0.4)
     interval = timer.end()
     print('Time: {:.2f}s, Detect Objects: {:d}.'.format(interval, labels.size(0)))
     for i in range(boxes.size(0)):
         box = boxes[i, :]
-        label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
+        label = f"{detector.class_names[labels[i]]}: {probs[i]:.2f}"
         cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
 
         cv2.putText(
---
+++ wagon_tracking/detection.py
@@ -0,0 +1,69 @@
+import cv2
+
+from vision.ssd.mobilenet_v2_ssd_lite import (
+    create_mobilenetv2_ssd_lite,
+    create_mobilenetv2_ssd_lite_predictor,
+)
+from vision.ssd.mobilenetv1_ssd import (
+    create_mobilenetv1_ssd,
+    create_mobilenetv1_ssd_predictor,
+)
+from vision.ssd.mobilenetv1_ssd_lite import (
+    create_mobilenetv1_ssd_lite,
+    create_mobilenetv1_ssd_lite_predictor,
+)
+from vision.ssd.squeezenet_ssd_lite import (
+    create_squeezenet_ssd_lite,
+    create_squeezenet_ssd_lite_predictor,
+)
+from vision.ssd.vgg_ssd import create_vgg_ssd, create_vgg_ssd_predictor
+
+
+class WagonDetector:
+    def __init__(self, net_type, label_path, model_path):
+        self.class_names = ['BACKGROUND'] + [
+            name.strip() for name in open(label_path).readlines()
+        ]
+
+        if net_type == 'vgg16-ssd':
+            self.net = create_vgg_ssd(len(self.class_names), is_test=True)
+        elif net_type == 'mb1-ssd':
+            self.net = create_mobilenetv1_ssd(len(self.class_names), is_test=True)
+        elif net_type == 'mb1-ssd-lite':
+            self.net = create_mobilenetv1_ssd_lite(len(self.class_names), is_test=True)
+        elif net_type == 'mb2-ssd-lite':
+            self.net = create_mobilenetv2_ssd_lite(len(self.class_names), is_test=True)
+        elif net_type == 'sq-ssd-lite':
+            self.net = create_squeezenet_ssd_lite(len(self.class_names), is_test=True)
+        else:
+            raise RuntimeError(
+                "The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite."
+            )
+        self.net.load(model_path)
+
+        if net_type == 'vgg16-ssd':
+            self.predictor = create_vgg_ssd_predictor(self.net, candidate_size=200)
+        elif net_type == 'mb1-ssd':
+            self.predictor = create_mobilenetv1_ssd_predictor(
+                self.net, candidate_size=200
+            )
+        elif net_type == 'mb1-ssd-lite':
+            self.predictor = create_mobilenetv1_ssd_lite_predictor(
+                self.net, candidate_size=200
+            )
+        elif net_type == 'mb2-ssd-lite':
+            self.predictor = create_mobilenetv2_ssd_lite_predictor(
+                self.net, candidate_size=200
+            )
+        elif net_type == 'sq-ssd-lite':
+            self.predictor = create_squeezenet_ssd_lite_predictor(
+                self.net, candidate_size=200
+            )
+        else:
+            raise RuntimeError(
+                "The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite."
+            )
+
+    def run(self, image, top_k, prob_threshold):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return self.predictor.predict(image, top_k, prob_threshold)
