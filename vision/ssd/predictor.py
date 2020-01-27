8c326812f0414a7caa39e5d2bb9b546c46303038
Camilo Gonçalves <camilolgon@gmail.com>
2020-01-27 11:01:17 -0300

    Supressing inference time text.

--- vision/ssd/predictor.py
+++ vision/ssd/predictor.py
@@ -35,7 +35,7 @@ class Predictor:
         with torch.no_grad():
             self.timer.start()
             scores, boxes = self.net.forward(images)
-            print("Inference time: ", self.timer.end())
+            # print("Inference time: ", self.timer.end())
         boxes = boxes[0]
         scores = scores[0]
         if not prob_threshold:
