bb6882ddaaecafa81b3c5ae6b5082d6f30a091dc
Camilo Gonçalves <camilolgon@gmail.com>
2020-01-29 12:43:28 -0300

    Refactoring some code.

--- wagon_tracking/detection.py
+++ wagon_tracking/detection.py
@@ -25,47 +25,45 @@ class WagonDetector:
             name.strip() for name in open(label_path).readlines()
         ]
 
-        if net_type == 'vgg16-ssd':
-            self.net = create_vgg_ssd(len(self.class_names), is_test=True)
-        elif net_type == 'mb1-ssd':
-            self.net = create_mobilenetv1_ssd(len(self.class_names), is_test=True)
-        elif net_type == 'mb1-ssd-lite':
-            self.net = create_mobilenetv1_ssd_lite(len(self.class_names), is_test=True)
-        elif net_type == 'mb2-ssd-lite':
-            self.net = create_mobilenetv2_ssd_lite(len(self.class_names), is_test=True)
-        elif net_type == 'sq-ssd-lite':
-            self.net = create_squeezenet_ssd_lite(len(self.class_names), is_test=True)
-        else:
-            raise RuntimeError(
-                "The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite."
-            )
+        self.net = self._create_network(net_type)
         self.net.load(model_path)
 
-        if net_type == 'vgg16-ssd':
-            self.predictor = create_vgg_ssd_predictor(self.net, candidate_size=200)
-        elif net_type == 'mb1-ssd':
-            self.predictor = create_mobilenetv1_ssd_predictor(
-                self.net, candidate_size=200
-            )
-        elif net_type == 'mb1-ssd-lite':
-            self.predictor = create_mobilenetv1_ssd_lite_predictor(
-                self.net, candidate_size=200
-            )
-        elif net_type == 'mb2-ssd-lite':
-            self.predictor = create_mobilenetv2_ssd_lite_predictor(
-                self.net, candidate_size=200
-            )
-        elif net_type == 'sq-ssd-lite':
-            self.predictor = create_squeezenet_ssd_lite_predictor(
-                self.net, candidate_size=200
-            )
-        else:
-            raise RuntimeError(
-                "The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite."
-            )
+        self.predictor = self._create_predictor(net_type)
 
         self.top_k = top_k
         self.prob_threshold = prob_threshold
+
+    def _create_network(self, net_type):
+        if net_type == 'vgg16-ssd':
+            return create_vgg_ssd(len(self.class_names), is_test=True)
+        elif net_type == 'mb1-ssd':
+            return create_mobilenetv1_ssd(len(self.class_names), is_test=True)
+        elif net_type == 'mb1-ssd-lite':
+            return create_mobilenetv1_ssd_lite(len(self.class_names), is_test=True)
+        elif net_type == 'mb2-ssd-lite':
+            return create_mobilenetv2_ssd_lite(len(self.class_names), is_test=True)
+        elif net_type == 'sq-ssd-lite':
+            return create_squeezenet_ssd_lite(len(self.class_names), is_test=True)
+        else:
+            raise RuntimeError(
+                "The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite."
+            )
+
+    def _create_predictor(self, net_type):
+        if net_type == 'vgg16-ssd':
+            return create_vgg_ssd_predictor(self.net, candidate_size=200)
+        elif net_type == 'mb1-ssd':
+            return create_mobilenetv1_ssd_predictor(self.net, candidate_size=200)
+        elif net_type == 'mb1-ssd-lite':
+            return create_mobilenetv1_ssd_lite_predictor(self.net, candidate_size=200)
+        elif net_type == 'mb2-ssd-lite':
+            return create_mobilenetv2_ssd_lite_predictor(self.net, candidate_size=200)
+        elif net_type == 'sq-ssd-lite':
+            return create_squeezenet_ssd_lite_predictor(self.net, candidate_size=200)
+        else:
+            raise RuntimeError(
+                "The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite."
+            )
 
     def __call__(self, image):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
