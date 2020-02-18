1c7e3732af84ba4e0050f186fbcad336356f6d61
Camilo Gonçalves <camilolgon@gmail.com>
2020-02-17 22:01:29 -0300

    Enhanced version.

--- run_tracker.py
+++ run_tracker.py
@@ -2,10 +2,17 @@ import os
 import sys
 
 import cv2
+import numpy as np
+
+from vision.utils import Timer
 
 from wagon_tracking.detection import WagonDetector
+from wagon_tracking.restrictions import (
+    DetectionDistanceRestriction,
+    ROIRestriction,
+    TrajectoryProfileRestriction,
+)
 from wagon_tracking.tracking import WagonTracker
-from wagon_tracking.restrictions import TrajectoryProfileRestriction, ROIRestriction
 from wagon_tracking.transforms import DistortionRectifier
 from wagon_tracking.utils import get_realpath
 from wagon_tracking.videostream import VideoFileStream, VideoLiveStream
@@ -20,11 +27,12 @@ net_type = sys.argv[1]
 model_path = sys.argv[2]
 label_path = sys.argv[3]
 video_path = sys.argv[4]
-camera_parameters = get_realpath('resources/camera_parameters.pkl.gz')
 if len(sys.argv) > 5:
     camera_parameters = get_realpath(sys.argv[5])
+    transform = [DistortionRectifier(camera_parameters)]
+else:
+    transform = []
 
-transform = [DistortionRectifier(camera_parameters)]
 if os.path.exists(video_path):
     cap = VideoFileStream(
         video_path, queue_sz=64, transforms=transform
@@ -32,6 +40,7 @@ if os.path.exists(video_path):
 else:
     cap = VideoLiveStream(video_path, transforms=transform)  # capture from camera
 frame_time = int(1 / cap.get(cv2.CAP_PROP_FPS) * 1000)
+# cap.stream.set(cv2.CAP_PROP_POS_MSEC, 982000)
 cap.start()
 
 '''-------------------------- Test code --------------------------'''
@@ -44,14 +53,24 @@ detector = WagonDetector(net_type, label_path, model_path, prob_threshold=0.4)
 restrictions = [
     ROIRestriction((302, 273, 1579, 796)),
     TrajectoryProfileRestriction(
-        (0, 0, frame_width, frame_height), (0, 560), distance_threshold=20
+        (0, 0, frame_width, frame_height), (0, frame_height // 2), distance_threshold=20
     ),
+    DetectionDistanceRestriction((1100, 1600), (200, 500)),
 ]
-tracker = WagonTracker(detector, frame_width // 2, restrictions=restrictions)
+tracker = WagonTracker(
+    detector,
+    frame_width // 2,
+    restrictions=restrictions,
+    video_fps=cap.get(cv2.CAP_PROP_FPS),
+    target_fps=30.0,
+)
+
+timer = Timer()
 
 cv2.namedWindow('annotated', cv2.WINDOW_NORMAL)
 
 while cap.more():
+    timer.start()
     orig_image = cap.read()
     if orig_image is None:
         continue
@@ -77,6 +96,7 @@ while cap.more():
         4,
     )
 
+    last_center = None
     if len(tracking_info) != 0:
         for id, (box, label) in tracking_info.items():
             label = f"{detector.class_names[label]}: {id}"
@@ -94,11 +114,39 @@ while cap.more():
                 1,  # font scale
                 (255, 0, 255),
                 2,
-            )  # line type
-            cv2.circle(orig_image, (int(center[0]), int(center[1])), 3, (0, 0, 255), 3)
+            )
+
+            cv2.circle(orig_image, tuple(center), 3, (0, 0, 255), 3)
+            cv2.putText(
+                orig_image,
+                str(center),
+                (int(center[0]), int(center[1] + 30)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,  # font scale
+                (255, 0, 255),
+                2,
+            )
+            if last_center is not None:
+                distance = np.linalg.norm(center - last_center)
+                cv2.line(
+                    orig_image, tuple(center), tuple(last_center), (0, 255, 255), 4
+                )
+                cv2.putText(
+                    orig_image,
+                    str(distance),
+                    (int(center[0]), int(center[1]) - 20),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,  # font scale
+                    (255, 0, 255),
+                    2,
+                )
+            last_center = center
 
     cv2.imshow('annotated', orig_image)
-    k = cv2.waitKey(frame_time // 4) & 0xFF
+
+    end_time = timer.end() * 1e3
+    wait_time = int(np.clip((frame_time - end_time) / 4, 1, frame_time))
+    k = cv2.waitKey(wait_time) & 0xFF
     if k == ord('q') or k == 27:
         break
 cv2.destroyAllWindows()
