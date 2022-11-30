24f53c60db55387f71252e2364271af84a876cb4
Anton Makarenko <makarenko.antonf2c@gmail.com>
2022-11-30 16:55:36 +0200

    add script for removing items without bounding boxes from a dataset

---
+++ remove_items_without_bboxes.py
@@ -0,0 +1,26 @@
+import xml.etree.ElementTree as ET
+
+files = ['trainval.txt', 'test.txt']
+
+for file in files:
+    image_ids = list()
+    f = open(f'dataset/soda10m_aspascal/ImageSets/Main/{file}')
+    for line in f:
+        image_ids.append(line.rstrip())
+
+    ids_to_remove = list()
+    for image_id in image_ids:
+        annotation_file = f'dataset/soda10m_aspascal/Annotations/{image_id}.xml'
+        objects = ET.parse(annotation_file).findall("object")
+
+        if len(objects) == 0:
+            ids_to_remove.append(image_id)
+
+    outfile = open(f'dataset/soda10m_aspascal/ImageSets/Main/{file}',
+                   'w')
+
+    for image_id in image_ids:
+        if image_id not in ids_to_remove:
+            outfile.write(image_id + '\n')
+
+    print(f'{len(ids_to_remove)} items removed from {file}')
